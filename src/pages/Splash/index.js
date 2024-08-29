e241f1b0c2aeb8c28560cc566ed69007c7d3870c
topdeveloper1219 <topdeveloper1219@gmail.com>
2024-08-30 03:03:07 +0900

    ===========

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -45,7 +45,7 @@ const Splash = () => {
                 navigator("/login");
               }}
             >
-              Loginhaha
+              Loginhaha========
             </div>
             <div
               className="w-80 h-14 rounded-full justify-center text-center flex text-[#FC6011] items-center text-base bg-white hand"
